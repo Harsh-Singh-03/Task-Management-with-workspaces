17e5f45d876373833684a1aac86f303597f11e88
Harsh Singh <harshnov802@gmail.com>
2023-12-15 14:05:45 +0530

    updating next confugiration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    esmExternals: "loose", // <-- add this
+    serverActions: true,
+    serverComponentsExternalPackages: ["mongoose"] // <-- and this
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
     images: {
       remotePatterns: [
         {
